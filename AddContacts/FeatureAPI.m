b605c10576350f8579b194fc54138f28d73d151d
BandoKal <jason.bandy77@gmail.com>
2014-05-14 12:16:27 -0500

    adding skeleton implementations to the API

--- AddContacts/FeatureAPI.m
+++ AddContacts/FeatureAPI.m
@@ -14,4 +14,35 @@
 
 @implementation FeatureAPI
 
+
+// public facing initializer TODO: May make this apart of the static accessor if not inited just init then.
+-(BOOL)initializeFeatureAPI{
+    return NO;
+}
+
+// singleton accessor
++(FeatureAPI*)singleFeatureAPI {
+    return [[FeatureAPI alloc]init];
+}
+
+// Add Contact Feature Set
+-(BOOL)requestAccessToABBook {
+    return NO;
+}
+-(void)addContacts:(NSArray*)contactsToAdd withCompletionBlock:(APICompletionBlock)completionBlock {
+    
+}
+-(void)removeAllContactsWithCompletionBlock:(APICompletionBlock)completionBlock{
+    
+}
+
+// Add Photos Feature Set
+-(void)addPhotos:(NSArray*) imagesToAdd toAlbumName:(NSString*)albumName withCompletionBlock:(APICompletionBlock)completionBlock{
+    
+}
+// This API call will use the random image generator
+-(void)addRandomPhotosWithCount:(NSUInteger*)imageCount toAlbumName:(NSString*)albumName withCompletionBlock:(APICompletionBlock)completionBlock{
+    
+}
+
 @end
